a0a01f6a95f10d85bb78492f7316a5485fa1564b
Jtave01 <j0a0vtorcat@gmail.com>
2025-03-29 02:42:58 -0300

    teste

--- tipos-variaveis/src/tiposvariavies.java
+++ tipos-variaveis/src/tiposvariavies.java
@@ -1,5 +1,14 @@
 public class tiposvariavies {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMininimo = 1212.00;
+        //byte idade =20;
+        //short ano = 2023;
+        //long cpf = 12345678901L;
+        //float pi = 3.14f;
+        
+        
+    
+        System.out.println("Salario minimo e " + salarioMininimo);
+
     }
 }
