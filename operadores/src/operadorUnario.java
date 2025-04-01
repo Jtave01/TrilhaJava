7e4ada57cd683cf9784b4e609136f256a46a4dee
Jtave01 <j0a0vtorcat@gmail.com>
2025-04-01 12:05:44 -0300

    Descrição das alterações

---
+++ operadores/src/operadorUnario.java
@@ -0,0 +1,16 @@
+public class operadorUnario {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+
+        numero = numero * -1;
+        
+        System.out.println(- numero);
+
+        System.out.println(numero);
+
+        
+    }
+    
+}
